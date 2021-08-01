434c16f256baee11c4431ed492e54f5694022ee6
marlon santana <marlonnig70@gmail.com>
2021-08-01 10:22:30 -0300

    create Home

---
+++ restaurants-search-boilerplate/src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+    colors:{
+    primary: '#6200ee',
+    background: '#0000014',
+    text: '#00000B3',
+    sucess: '#135E01',
+},
+fonts: {
+    fontFamily: 'Roboto, sans-serif',
+ },
+};
+
+export default theme;
