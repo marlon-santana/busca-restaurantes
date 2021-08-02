bc1ea24d1d5dd237b297902a60fcddac092f1296
marlon santana <marlonnig70@gmail.com>
2021-08-02 20:19:16 -0300

    add reset

--- restaurants-search-boilerplate/src/pages/home/styles.js
+++ restaurants-search-boilerplate/src/pages/home/styles.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
  export const Container = styled.div`
-background: blue;
-color: white;
-border: solid 2px;
-width: 500px
+background-color: ${(props) => props.theme.colors.primary};
+
 `;
