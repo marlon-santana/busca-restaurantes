ce245422078185233b387f190aab9bed58a7e578
marlon santana <marlonnig70@gmail.com>
2021-08-03 22:44:59 -0300

    add material/icon

--- restaurants-search-boilerplate/src/pages/home/index.jsx
+++ restaurants-search-boilerplate/src/pages/home/index.jsx
@@ -2,6 +2,7 @@ import React, { useState }  from 'react';
 import logo from '../../assets/logo.svg';
 import { Container, Search } from './styles';
 import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
 
 
@@ -12,9 +13,10 @@ export function Home() {
             <Search>
                 <img src={logo} alt="texto da logo"></img>
                 <TextField
-                label='Pesquisar'
+                label='Pesquisar Restaurantes'
                 outlined
                // onTrailingIconSelect={() => this.setState({value: ''})}
+               trailingIcon={<MaterialIcon role="button" icon="search" />}
                 ><Input
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
