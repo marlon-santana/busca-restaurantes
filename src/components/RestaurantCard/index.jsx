833dc99c9a2a564fc44e2e0f63ae4e42e173dc8e
marlon santana <marlonnig70@gmail.com>
2021-08-08 09:12:55 -0300

    styles of components

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Restaurant } from './style';
+import { Restaurant, RestaurantInfo, Title, Address } from './style';
 
 
 export function RestaurantCard () {
     return (
-        <Restaurant />
+        <Restaurant>
+            <RestaurantInfo>
+                <Title>Nome do restaurante</Title>
+                <p>Avaliação</p>
+                <Address>Rua:Paulista n 27 Rio de Janeiro.</Address>
+            </RestaurantInfo>
+        </Restaurant >
+
     );
 }
 
--- src/components/RestaurantCard/style.js
+++ src/components/RestaurantCard/style.js
@@ -14,3 +14,27 @@ border-left: 5px solid transparent;
     border-left: 5px solid purple;
 }
 `;
+export const RestaurantInfo = styled.div`
+display: flex;
+flex-direction: column;
+
+`;
+
+export const Title = styled.span`
+font-family: Roboto, sans-serif;
+font-size: 20px;
+font-weight: bold;
+color: #696969;
+line-height: 29px;
+margin-bottom:10px;
+
+`;
+
+export const Address = styled.span`
+font-family: Roboto, sans-serif;
+font-size: 16px;
+color: #696969;
+line-height: 19px;
+margin-bottom:10px;
+
+`;
